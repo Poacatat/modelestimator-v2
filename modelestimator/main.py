b2a61119e524c4bab6aedc6ee53690a738f89b01
arvestad <arve@csc.kth.se>
2019-03-20 11:18:56 +0100

    Adapting argparse for option handling. Moving the controller function here to simplify program reading.

--- modelestimator/main.py
+++ modelestimator/main.py
@@ -1,6 +1,12 @@
+import argparse
 import sys
-from modelestimator._parse_arguments import parse_arguments
-from modelestimator._controller import controller
+
+from modelestimator.version import __version__
+
+from modelestimator._bw_estimator.bw_estimator import bw_estimator
+from modelestimator._handle_input.handle_input_file import handle_input_file
+from modelestimator._bootstraper.bootstraper import bootstraper
+
 
 USAGE_STRING ="""Usage: python -m modelestimator <format> <options> infiles
 
@@ -13,27 +19,89 @@ Options:
     -bootstrap or -b <r> Perform bootstrapping on multialignment with <r> resamplings.
                          Only one infile should be given in this mode. Returns
                          bootstrap norm.
+"""
 
+description='''
+Infer a amino-acid replacement-rate matrix from one or more protein multisequence
+alignments. The output is a rate matrix and an associated steady-state amino-acid
+distribution vector.
+'''
+example_usage='''
 Example usage:
     modelestimator fasta -t 0.001 file1.fa file2.fa file3.fa
-    modelestimator fasta -b 200 file.fa"""
+    modelestimator fasta -b 200 file.fa
+'''
+
+def setup_argument_parsing():
+    '''
+    Create an argument parser.
+    '''
+    ap = argparse.ArgumentParser(description=description, epilog=example_usage,
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
+    ap.add_argument('infiles', nargs='+', help="One or more infiles, containing protein multialignments in FASTA format or chosen according to the -f/--format option.")
+    ap.add_argument('--version', action='version', version='%(prog)s ' + __version__)
+    ap.add_argument('-f', '--format', choices=['fasta', 'clustal', 'nexus', 'phylip', 'stockholm'], default='fasta',
+                    help="Specify sequence format of input files. Default: %(default)s")
+    # ap.add_argument('-f', '--format', choices=['guess', 'fasta', 'clustal', 'nexus', 'phylip', 'stockholm'], default='guess',
+    #                 help="Specify what sequence type to assume. Be specific if the file is not recognized automatically. When reading from stdin, the format is always guessed to be FASTA. Default: %(default)s")
+    ap.add_argument('-t', '--threshold', type=float, default=0.001,
+                    help="Stop when consecutive iterations do not change by more than THRESHOLD. Default: %(default)f.")
+    ap.add_argument('-b', '--bootstrap', type=int, default=None,
+                    help="Perform bootstrapping on multialignment with BOOTSTRAP resamplings. Only one infile should be given in this mode. Returns bootstrap norm.")
+    return ap
+
+
+def controller(msa_format, bootstraps, threshold, filenames):
+    '''
+    Control the actions of the program.
+    In: 
+        msa_format - One of the allowed input formats, recognised by BioPython.
+        bootstraps - None or an integer for the number of replacements in the bootstrap procedure.
+        threshold  - Determines when to stop iterations, the minimum improvement before termination.
+        
+    '''
+    MULTIALIGNMENT_LIST = []
+    
+    for FILE in filenames:
+        MULTIALIGNMENT = handle_input_file(FILE, msa_format)
+        MULTIALIGNMENT_LIST.append(MULTIALIGNMENT) 
+        
+    if threshold == None:
+        threshold = 0.001
+    
+    if bootstraps:
+        MULTIALIGNMENT = MULTIALIGNMENT_LIST[0]
+        BOOTSTRAP_NORM,_ = bootstraper(RESAMPLINGS, threshold, MULTIALIGNMENT)
+        OUTPUT_STRING = "Bootstrap norm = " + str(BOOTSTRAP_NORM)
+    else:
+        Q, EQ = bw_estimator(threshold, MULTIALIGNMENT_LIST)
+
+        # Used to output the Q matrix:
+        # OUTPUT_STRING = "# Q =\n" + str(Q) + "\n# EQ =\n" + str(EQ)   
+
+        # ...but if outputting the R matrix (which is symmetrix and q_ij = r_ij*p_j)
+        # we get compatibility with other tools, like PhyML.
+        OUTPUT_STRING = ''
+        for row in range(1,20):
+            for col in range(0, row):
+                r = Q[row, col] / EQ[col]
+                OUTPUT_STRING += f'{r:10.5} '
+            OUTPUT_STRING += '\n'
+
+        EQ_STR = ''
+        for elem in range(0,20):
+            EQ_STR += f'{EQ[elem]:10.5} '
+
+        OUTPUT_STRING += '\n' + EQ_STR
+
+    return OUTPUT_STRING
 
 def main():
-    ARGUMENT_LIST = sys.argv[1:]
-      
-    if len(ARGUMENT_LIST) == 0:
-        print(USAGE_STRING)
-        exit()
+    ap = setup_argument_parsing()
+    args = ap.parse_args()
             
     try:
-        FORMAT, BOOTSTRAP, RESAMPLINGS, THRESHOLD, FILE_NAMES = parse_arguments(ARGUMENT_LIST)
-    except Exception as e:
-        print("Wrong format on input\n")
-        print(USAGE_STRING)
-        exit()
-        
-    try:
-        OUTPUT_STRING = controller(FORMAT, BOOTSTRAP, RESAMPLINGS, THRESHOLD, FILE_NAMES)
+        OUTPUT_STRING = controller(args.format, args.bootstrap, args.threshold, args.infiles)
     except Exception as e:
         print("Error: ", e)
         exit()
