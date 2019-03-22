922f275a54b3cc50c01338c0d1125903fd78f9c3
arvestad <arve@csc.kth.se>
2019-03-22 16:06:11 +0100

    Added support for choosing output format. See also io.py.

--- modelestimator/main.py
+++ modelestimator/main.py
@@ -4,7 +4,7 @@ import sys
 from modelestimator.version import __version__
 
 from modelestimator._bw_estimator.bw_estimator import bw_estimator
-from modelestimator._handle_input.handle_input_file import handle_input_file
+from modelestimator.io import handle_input_file, format_model_output
 from modelestimator._bootstraper.bootstraper import bootstraper
 
 
@@ -40,18 +40,20 @@ def setup_argument_parsing():
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
     ap.add_argument('infiles', nargs='+', help="One or more infiles, containing protein multialignments in FASTA format or chosen according to the -f/--format option.")
     ap.add_argument('--version', action='version', version='%(prog)s ' + __version__)
+    ap.add_argument('-a', '--application', choices=['iqtree', 'matlab', 'mrbayes', 'octave', 'paml', 'phyml'], default='paml',
+                    help="Choose output format to suit the application you want to use for inference. The 'iqtree', 'paml' and 'phyml' options are identical. The 'matlab' and 'octave' optins are for import into MatLab-compatible programs and are presenting the actual Q matrix rather than the R matrix used by PAML/PhyML, etc. Default: %(default)s")
     ap.add_argument('-f', '--format', choices=['fasta', 'clustal', 'nexus', 'phylip', 'stockholm'], default='fasta',
                     help="Specify sequence format of input files. Default: %(default)s")
     # ap.add_argument('-f', '--format', choices=['guess', 'fasta', 'clustal', 'nexus', 'phylip', 'stockholm'], default='guess',
     #                 help="Specify what sequence type to assume. Be specific if the file is not recognized automatically. When reading from stdin, the format is always guessed to be FASTA. Default: %(default)s")
-    ap.add_argument('-t', '--threshold', type=float, default=0.001,
-                    help="Stop when consecutive iterations do not change by more than THRESHOLD. Default: %(default)f.")
-    ap.add_argument('-b', '--bootstrap', type=int, default=None,
-                    help="Perform bootstrapping on multialignment with BOOTSTRAP resamplings. Only one infile should be given in this mode. Returns bootstrap norm.")
+    ap.add_argument('-t', '--threshold', type=float, metavar='T', default=0.001,
+                    help="Stop when consecutive iterations do not change by more than T. Default: %(default)g")
+    ap.add_argument('-b', '--bootstrap', type=int, metavar='N', default=None,
+                    help="Perform bootstrapping on multialignment with N resamplings. Only one infile should be given in this mode. Returns bootstrap norm.")
     return ap
 
 
-def controller(msa_format, bootstraps, threshold, filenames):
+def controller(msa_format, output_format, bootstraps, threshold, filenames):
     '''
     Control the actions of the program.
     In: 
@@ -60,50 +62,33 @@ def controller(msa_format, bootstraps, threshold, filenames):
         threshold  - Determines when to stop iterations, the minimum improvement before termination.
         
     '''
-    MULTIALIGNMENT_LIST = []
+    multialignment_list = []
     
-    for FILE in filenames:
-        MULTIALIGNMENT = handle_input_file(FILE, msa_format)
-        MULTIALIGNMENT_LIST.append(MULTIALIGNMENT) 
+    for file in filenames:
+        multialignment = handle_input_file(file, msa_format)
+        multialignment_list.append(multialignment) 
         
     if threshold == None:
         threshold = 0.001
     
     if bootstraps:
-        MULTIALIGNMENT = MULTIALIGNMENT_LIST[0]
-        BOOTSTRAP_NORM,_ = bootstraper(RESAMPLINGS, threshold, MULTIALIGNMENT)
-        OUTPUT_STRING = "Bootstrap norm = " + str(BOOTSTRAP_NORM)
+        multialignment = multialignment_list[0]
+        bootstrap_norm,_ = bootstraper(bootstraps, threshold, multialignment)
+        output_string = "Bootstrap norm = " + str(bootstrap_norm)
     else:
-        Q, EQ = bw_estimator(threshold, MULTIALIGNMENT_LIST)
+        Q, EQ = bw_estimator(threshold, multialignment_list)
+        output_string = format_model_output(Q, EQ, output_format)
 
-        # Used to output the Q matrix:
-        # OUTPUT_STRING = "# Q =\n" + str(Q) + "\n# EQ =\n" + str(EQ)   
-
-        # ...but if outputting the R matrix (which is symmetrix and q_ij = r_ij*p_j)
-        # we get compatibility with other tools, like PhyML.
-        OUTPUT_STRING = ''
-        for row in range(1,20):
-            for col in range(0, row):
-                r = Q[row, col] / EQ[col]
-                OUTPUT_STRING += f'{r:10.5} '
-            OUTPUT_STRING += '\n'
-
-        EQ_STR = ''
-        for elem in range(0,20):
-            EQ_STR += f'{EQ[elem]:10.5} '
-
-        OUTPUT_STRING += '\n' + EQ_STR
-
-    return OUTPUT_STRING
+    return output_string
 
 def main():
     ap = setup_argument_parsing()
     args = ap.parse_args()
             
     try:
-        OUTPUT_STRING = controller(args.format, args.bootstrap, args.threshold, args.infiles)
+        output_string = controller(args.format, args.application, args.bootstrap, args.threshold, args.infiles)
     except Exception as e:
         print("Error: ", e)
         exit()
 
-    print(OUTPUT_STRING)
+    print(output_string)
