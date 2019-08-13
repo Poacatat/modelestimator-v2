b6b673e2c9bff79f708007929dfb2e406fc16d95
arvestad <arve@csc.kth.se>
2019-08-13 08:40:14 +0200

    v2.1.1

--- modelestimator/version.py
+++ modelestimator/version.py
@@ -1,6 +1,7 @@
-__version__ = '2.1'
+__version__ = '2.1.1'
 
 ### Version history
 #
-# 2.0 - Ruben Riddeström's BSc thesis work
-# 2.1 - Lars' adaptions
+# 2.0   - Ruben Riddeström's BSc thesis work
+# 2.1   - Lars' adaptions
+# 2.1.1 - Updated README, corrected URL,
