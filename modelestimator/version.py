7e45319627911809d2ac1166a31661ed13ee3369
arvestad <arve@csc.kth.se>
2019-10-03 11:22:16 +0200

    Bumping version number for patch

--- modelestimator/version.py
+++ modelestimator/version.py
@@ -1,4 +1,4 @@
-__version__ = '2.1.2'
+__version__ = '2.1.3'
 
 ### Version history
 #
@@ -6,3 +6,4 @@ __version__ = '2.1.2'
 # 2.1   - Lars' adaptions
 # 2.1.1 - Updated README, corrected URL,
 # 2.1.2 - Fixed content type on README for PyPi
+# 2.1.3 - Updated unit tests and corrected Octave output
