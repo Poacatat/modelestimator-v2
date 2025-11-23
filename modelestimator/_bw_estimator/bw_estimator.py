caac4d901bc38b152ca8e403c9cd682f4f9a953d
Your Name <you@example.com>
2025-11-23 14:27:40 +0100

    yehaw!

--- modelestimator/_bw_estimator/bw_estimator.py
+++ modelestimator/_bw_estimator/bw_estimator.py
@@ -27,7 +27,7 @@ def bw_estimator(threshold, msa_list, compare_indels_flag = False):
     #    two2 = time.perf_counter()
     #    aggregated_count_matrix_list.extend(count_matrix_list)
     #    three3 = time.perf_counter()
-    aggregated_count_matrix_list = np.load("../a_c_m_l.npy")
+    aggregated_count_matrix_list = np.load("/home/oscar/a_c_m_l.npy")
     #so there we go, one stp closer to the truth. 
     
     Q, eq = calculate_q_eq(aggregated_count_matrix_list, threshold)
